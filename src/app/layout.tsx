e48183a08c1eac59dbe39a020e60ac3453cf47b6
asojk <113611085+asojk@users.noreply.github.com>
2024-10-03 08:30:33 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
+import { FloatingDockDemo } from "@/components/FloatingDock";
+import { Profile } from "@/components/Proifile";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,13 +31,14 @@ export default function RootLayout({
           "flex antialiased h-screen overflow-hidden bg-gray-100"
         )}
       >
+          <Profile />
         <Sidebar />
         <div className="lg:pl-2 lg:pt-2 bg-gray-100 flex-1 overflow-y-auto">
           <div className="flex-1 bg-white min-h-screen lg:rounded-tl-xl border border-transparent lg:border-neutral-200 overflow-y-auto">
             {children}
             <Footer />
           </div>
-        </div>
+          <FloatingDockDemo />        </div>
       </body>
     </html>
   );
