cc0aee95303af42b169c3b0e561c5bc238f0a14b
asojk <113611085+asojk@users.noreply.github.com>
2024-10-03 10:52:57 -0500

    Fltnav layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
-import { FloatingBottomNav } from "@/components/FloatingBottomNav";
+import FloatingBottomNav from "@/components/FloatingBottomNav";
 
 const inter = Inter({
   subsets: ["latin"],
