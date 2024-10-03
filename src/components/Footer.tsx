8bec3657f9ca65b2287b3a07f2bae0256a2e2f24
asojk <113611085+asojk@users.noreply.github.com>
2024-10-03 08:35:35 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="p-4 text-center justify-center text-xs text-neutral-500 border-t border-neutral-100">
-      <span className="font-semibold">{new Date().getFullYear()} </span>
-      &#8212; Built by Manu Arora
+    Built by <a className='italic text-violet-500' href='https://selynt.com'>Selynt.com</a><span className="pl-1">{new Date().getFullYear()} </span>
     </div>
   );
 };
