3c18b3d4f5b3b8fd9b56bc9e28eab4886c98a921
asojk <113611085+asojk@users.noreply.github.com>
2024-10-03 09:06:40 -0500

    Revert utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,3 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export const isMobile = () => {
   if (typeof window === "undefined") return false;
   const width = window.innerWidth;
